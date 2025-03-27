63a5911213bbe64994eceea8545413df2858fd86
ca8z <abody8080m@gmail.com>
2025-03-27 19:33:37 +0300

    رسالة التحديث

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,12 @@
+import Header from "../../components/Header";
+import { Box } from "@mui/material"
+
 const Dashboard = () => {
-    return <div>Dashboard</div>
+    return <Box m='20px'>
+        <Box display='flex' justifyContent="space-between" alignItems="center">
+        <Header title='DASHBORAD' subtitle='Welocme to your dashborad' />
+        </Box>
+        </Box>
 }
 
 export default Dashboard;
